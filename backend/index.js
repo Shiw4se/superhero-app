4b09c5db0cf6f2ee35a7b700448882c7388c9dca
Shiw4se <ausenko476@gmail.com>
2026-01-14 11:39:43 +0200

    Setup mongodb connection and hero model

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+﻿require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Подключение к БД
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log(' MongoDb successfully connected!'))
+    .catch((err) => console.error('Error', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
